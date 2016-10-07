35e97dacc09d8d33f7d47fc378c00a091173035d
c650 <charlesbailey@protonmail.com>
2016-10-07 14:41:14 -0400

    add sum vs xor

---
+++ Algorithms/Bit Manipulation/sum-vs-xor.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    unsigned long long n; std::cin >> n;
+
+    unsigned long long bin_zeros = 0;
+    for (unsigned long long i = 1; i > 0 && i < n; i = i << 1)
+        bin_zeros += !(n & i);
+
+    std::cout << (unsigned long long)pow(2, bin_zeros) << std::endl;
+    return 0;
+}
