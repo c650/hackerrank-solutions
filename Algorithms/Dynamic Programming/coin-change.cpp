6ec46b3aa3c1ff64e00c9ae834218feb450d5996
Charles <c650@users.noreply.github.com>
2017-03-16 18:16:49 -0400

    fix spacing

--- Algorithms/Dynamic Programming/coin-change.cpp
+++ Algorithms/Dynamic Programming/coin-change.cpp
@@ -5,34 +5,33 @@
 static std::vector<int> coins;
 
 static long long do_counts(const int& target) {
-	    std::vector<std::vector<long long>> T(coins.size(), std::vector<long long>(target + 1, 1));
+	std::vector<std::vector<long long>> T(coins.size(), std::vector<long long>(target + 1, 1));
 		    
-		    for (int j = 1; j < T[0].size(); ++j) {
-				        if (j % coins[0])
-							            T[0][j] = 0;
-						    }
+	for (int j = 1; j < T[0].size(); ++j) {
+		if (j % coins[0])
+			T[0][j] = 0;
+	}
 			    
-			    for (int i = 1, j; i < T.size(); ++i) {
-					        for (j = 0; j < T[i].size(); ++j) {
-								            if (j >= coins[i])
-												                T[i][j] = T[i-1][j] + T[i][j - coins[i]];
-											            else
-															                T[i][j] = T[i-1][j];
-														        }
-							    }
-				    return T[coins.size()-1][target];
+	for (int i = 1, j; i < T.size(); ++i) {
+		for (j = 0; j < T[i].size(); ++j) {
+			if (j >= coins[i])
+				T[i][j] = T[i-1][j] + T[i][j - coins[i]];
+			else
+			    T[i][j] = T[i-1][j];
+		}
+	return T[coins.size()-1][target];
 }
 
 int main() {
-	    int n,m;
-		    std::cin >> n >> m;
-			    coins.resize(m);
-				    for (auto& c : coins)
-						        std::cin >> c;
+	int n,m;
+	std::cin >> n >> m;
+	coins.resize(m);
+	for (auto& c : coins)
+		std::cin >> c;
 					    
-					    std::sort(coins.begin(), coins.end());    
-						    std::cout << do_counts(n) << '\n';
+	std::sort(coins.begin(), coins.end());    
+	std::cout << do_counts(n) << '\n';
 							    
-							    return 0;
+	return 0;
 }
 
